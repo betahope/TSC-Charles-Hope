e15f85e918718d6468fd1e3e6319e65da5ec55ec
Developer Hope <me@devhope.co>
2021-06-04 13:04:57 +0000

    Improved emailjs function to not send if any form field is empty

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,23 +1,25 @@
 function sendMail(contactForm) {
-  emailjs
-    .send("gmail", "tsc_form", {
-      from_first_name: contactForm.firstName.value,
-      from_last_name: contactForm.lastName.value,
-      from_email: contactForm.emailAddress.value,
-      contact_message: contactForm.message.value,
-    })
-    .then(
-      function (response) {
-        console.log("SUCCESS", response);
-      },
-      function (error) {
-        console.log("FAILED", error);
-      }
-    );
+  if ( document.getElementById("firstName").value && document.getElementById("lastName").value && document.getElementById("emailAddress").value && document.getElementById("message").value ) {
+    emailjs
+      .send("gmail", "tsc_form", {
+        from_first_name: contactForm.firstName.value,
+        from_last_name: contactForm.lastName.value,
+        from_email: contactForm.emailAddress.value,
+        contact_message: contactForm.message.value,
+      })
+      .then(
+        function (response) {
+          console.log("SUCCESS", response);
+        },
+        function (error) {
+          console.log("FAILED", error);
+        }
+      );
 
-  // document.getElementById("contactForm").reset();
+    // document.getElementById("contactForm").reset();
 
-  alert("Thanks! I'll get back to you soon. Have a good one!");
+    alert("Thanks! I'll get back to you soon. Have a good one!");
 
-  return false; // To block from loading a new page
+    return false; // To block from loading a new page
+  } else { return }
 }
