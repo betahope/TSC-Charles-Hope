85f4c737cb93a9d9b32f443c2d7a2b16f6b69d6d
Developer Hope <me@devhope.co>
2021-06-08 08:40:23 +0000

    Simplified contact form requesting minimum info for communication

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,9 +1,8 @@
 function sendMail(contactForm) {
-  if ( document.getElementById("firstName").value && document.getElementById("lastName").value && document.getElementById("emailAddress").value && document.getElementById("message").value ) {
+  if ( document.getElementById("firstName").value && document.getElementById("emailAddress").value && document.getElementById("message").value ) {
     emailjs
       .send("gmail", "tsc_form", {
         from_first_name: contactForm.firstName.value,
-        from_last_name: contactForm.lastName.value,
         from_email: contactForm.emailAddress.value,
         contact_message: contactForm.message.value,
       })
